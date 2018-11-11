5ac2282468b45f0cfd78f6b4176b1c578ff158ce
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2018-11-11 21:26:54 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div class="cert-cover">
+      <Cert1/>
+    </div>  
+    <div class="cert-cover">
+      <Cert2/>
+    </div> 
+    <div class="cert-cover">
+      <Cert3/>
+    </div>    
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cert1 from './components/Cert1'
+import Cert2 from './components/Cert2'
+import Cert3 from './components/Cert3'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    Cert1,
+    Cert2,
+    Cert3
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
+@import './assets/css/style.css';
 </style>
+
---
+++ src/components/Cert2.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-app>
+		<div class="cert2 cert">
+			<img class="bg" src="../assets/images/1.png"/>
+			<div class="content">
+				<div>
+					<h5>Certificate Qualification</h5>
+					<h6>Presented to</h6>
+					<h2>John Doe</h2>
+					<h6>For Participating in</h6>
+					<h4>The Name of Course</h4>
+					<h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus iure corrupti similique a recusandae fugit cupiditate harum nulla error consequatur eaque cumque assumenda excepturi, hic aspernatur quis ratione labore delectus.</h6>
+				</div>
+			</div>
+		</div>
+		
+	</v-app>
+</template>
